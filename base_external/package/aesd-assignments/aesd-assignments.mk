f04e2adbcdcd0cf5b49aaa6a0d5ec8d510981720
eopheim <78663392+eopheim@users.noreply.github.com>
2025-02-11 06:31:09 -0500

    referenced a3

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = c849e8008a71dbf13a6296f720e8640e60d6a5d1
+AESD_ASSIGNMENTS_VERSION = 43067c496dd1a1009d8f2b706fee6b5bc15b2cfd
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
